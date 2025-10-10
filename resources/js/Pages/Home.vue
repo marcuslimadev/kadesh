3a1f33dadd6666218b66f475598a72de702915de
Marcus André Lima de Lima <marcus.lima@hotmail.com.br>
2025-10-10 16:26:12 -0300

    Adiciona emoji no título da home para teste de deploy

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,7 +5,7 @@
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
         <div class="text-center">
           <h1 class="text-4xl md:text-6xl font-bold mb-6">
-            O futuro dos 
+            🚀 O futuro dos 
             <span class="bg-gradient-to-r from-yellow-400 to-orange-500 bg-clip-text text-transparent">
               projetos freelance
             </span>
